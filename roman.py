2351301c1fd204ce73960dbf8662462c514f4ba6
Richard Kasperowski <r@kasperowski.com>
2024-10-09 12:48:04 -0400

    Subtract Roman numerals

--- roman.py
+++ roman.py
@@ -1,13 +1,11 @@
 def convert(roman):
-    if roman == "IV":
-        return 4
-    elif roman == "IX":
-        return 9
-    elif roman == "XC":
-        return 90
-
     rdict_char = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+
+    es_resta = len(roman) == 2 and rdict_char[roman[0]] < rdict_char[roman[1]]
+    if es_resta:
+        return rdict_char[roman[1]] - rdict_char[roman[0]]
+
     total = 0
-    for c in roman:
+    for index, c in enumerate(roman):
         total += rdict_char[c]
     return total
