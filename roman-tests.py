20805b7837790c5733277b5eb87ba15c203fb6f0
Richard Kasperowski <r@kasperowski.com>
2024-11-13 11:48:25 -0500

    Add and subtract Roman numerals in the same number

--- roman-tests.py
+++ roman-tests.py
@@ -1,7 +1,7 @@
 import pytest
 from roman import convert
 
-@pytest.mark.parametrize("input, expected", [
+@pytest.mark.parametrize("roman, expected", [
     ("I", 1),
     ("V", 5),
     ("X", 10),
@@ -10,23 +10,31 @@ from roman import convert
     ("D", 500),
     ("M", 1000),
 ])
-def test_singulares(input, expected):
-    assert convert(input) == expected
+def test_singulares(roman, expected):
+    assert convert(roman) == expected
 
-@pytest.mark.parametrize("input, expected", [
+@pytest.mark.parametrize("roman, expected", [
     ("II", 2),
     ("III", 3),
     ("VI", 6),
     ("MDCCCLXXXII", 1882)
 ])
-def test_sumacion(input, expected):
-    assert convert(input) == expected
+def test_sumacion(roman, expected):
+    assert convert(roman) == expected
 
-@pytest.mark.parametrize("input, expected", [
+@pytest.mark.parametrize("roman, expected", [
     ("IV", 4),
     ("IX", 9 ),
     ("XC", 90)
 ])
-def test_resta(input, expected):
-    assert convert(input) == expected
+def test_resta(roman, expected):
+    assert convert(roman) == expected
 
+@pytest.mark.parametrize("roman, expected", [
+    ("XIV", 14),
+    ("XCI", 91),
+    ("MDCCCLXXXIV", 1884),
+    ("MMXXIV", 2024),
+])
+def test_adicion_y_resta_mixta(roman, expected):
+    assert convert(roman) == expected
--- roman.py
+++ roman.py
@@ -1,11 +1,15 @@
-def convert(roman):
-    rdict_char = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+def convert(roman: str) -> int:
+    single_digit_romans: dict[str, int] = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-    es_resta = len(roman) == 2 and rdict_char[roman[0]] < rdict_char[roman[1]]
-    if es_resta:
-        return rdict_char[roman[1]] - rdict_char[roman[0]]
-
-    total = 0
-    for index, c in enumerate(roman):
-        total += rdict_char[c]
+    total: int = 0
+    prev_value: int = 0
+    
+    char: str
+    for char in reversed(roman):
+        current_value: int = single_digit_romans[char]
+        if current_value >= prev_value:
+            total += current_value
+        else:
+            total -= current_value
+        prev_value = current_value
     return total
