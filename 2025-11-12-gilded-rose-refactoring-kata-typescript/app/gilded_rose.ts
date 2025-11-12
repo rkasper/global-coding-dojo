7319bdf5a25b9da369edd6e0b6d29823a017df58
rkasper <r@kasperowski.com>
2025-11-12 12:08:16 -0500

    Refactor

--- 2025-11-12-gilded-rose-refactoring-kata-typescript/app/gilded_rose.ts
+++ 2025-11-12-gilded-rose-refactoring-kata-typescript/app/gilded_rose.ts
@@ -19,51 +19,89 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
+            this.updateItem(this.items[i]);
         }
 
         return this.items;
+    }
+
+    private updateItem(item: Item): void {
+        if (this.isSulfuras(item)) {
+            this.updateSulfuras(item);
+        } else if (this.isAgedBrie(item)) {
+            this.updateAgedBrie(item);
+        } else if (this.isBackstagePass(item)) {
+            this.updateBackstagePass(item);
+        } else {
+            this.updateNormalItem(item);
+        }
+    }
+
+    private isSulfuras(item: Item): boolean {
+        return item.name === 'Sulfuras, Hand of Ragnaros';
+    }
+
+    private isAgedBrie(item: Item): boolean {
+        return item.name === 'Aged Brie';
+    }
+
+    private isBackstagePass(item: Item): boolean {
+        return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+    }
+
+    private updateSulfuras(item: Item): void {
+        // Sulfuras never changes
+    }
+
+    private updateAgedBrie(item: Item): void {
+        this.increaseQuality(item);
+        this.decreaseSellIn(item);
+
+        if (item.sellIn < 0) {
+            this.increaseQuality(item);
+        }
+    }
+
+    private updateBackstagePass(item: Item): void {
+        this.increaseQuality(item);
+
+        if (item.sellIn < 11) {
+            this.increaseQuality(item);
+        }
+
+        if (item.sellIn < 6) {
+            this.increaseQuality(item);
+        }
+
+        this.decreaseSellIn(item);
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private updateNormalItem(item: Item): void {
+        this.decreaseQuality(item);
+        this.decreaseSellIn(item);
+
+        if (item.sellIn < 0) {
+            this.decreaseQuality(item);
+        }
+    }
+
+    private increaseQuality(item: Item): void {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private decreaseQuality(item: Item): void {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    private decreaseSellIn(item: Item): void {
+        item.sellIn = item.sellIn - 1;
     }
 }
