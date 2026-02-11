3f18c12f1825d185513e9330d87af376e7eb1fb1
rkasper <r@kasperowski.com>
2026-02-11 03:39:41 -0500

    Complete level 2 of bank account kata

--- 2026-02-11/bank_account.ts
+++ 2026-02-11/bank_account.ts
@@ -1,9 +1,23 @@
+export interface Transaction {
+    type: string;
+    amount: number;
+    date: Date;
+    balanceAfter: number;
+}
+
 export class BankAccount {
     private balanceInCents: number = 0;
+    private transactions: Transaction[] = [];
 
     deposit(dollars: number) {
         this.assertPositiveAmount(dollars);
         this.balanceInCents += dollars * 100;
+        this.transactions.push({
+            type: "deposit",
+            amount: dollars,
+            date: new Date(),
+            balanceAfter: this.balanceInCents / 100
+        });
     }
 
     withdraw(dollars: number) {
@@ -13,10 +27,20 @@ export class BankAccount {
             throw new Error("Insufficient funds");
         }
         this.balanceInCents -= amountInCents;
+        this.transactions.push({
+            type: "withdrawal",
+            amount: dollars,
+            date: new Date(),
+            balanceAfter: this.balanceInCents / 100
+        });
     }
 
     get balance(): number {
         return this.balanceInCents / 100;
+    }
+
+    getTransactions(): Transaction[] {
+        return this.transactions;
     }
 
     private assertPositiveAmount(amount: number) {
--- 2026-02-11/bank_account_test.ts
+++ 2026-02-11/bank_account_test.ts
@@ -56,3 +56,95 @@ Deno.test(function withdraw_rejects_negative_amounts() {
   }, Error, "Amount must be positive");
 });
 
+Deno.test(function getTransactions_returns_deposit_transaction() {
+  const bankAccount = new BankAccount();
+  bankAccount.deposit(100);
+  const transactions = bankAccount.getTransactions();
+  assertEquals(transactions.length, 1);
+  assertEquals(transactions[0].type, "deposit");
+  assertEquals(transactions[0].amount, 100);
+});
+
+Deno.test(function getTransactions_returns_multiple_deposits() {
+  const bankAccount = new BankAccount();
+  bankAccount.deposit(100);
+  bankAccount.deposit(50);
+  bankAccount.deposit(25.50);
+  const transactions = bankAccount.getTransactions();
+  assertEquals(transactions.length, 3);
+  assertEquals(transactions[0].type, "deposit");
+  assertEquals(transactions[0].amount, 100);
+  assertEquals(transactions[1].type, "deposit");
+  assertEquals(transactions[1].amount, 50);
+  assertEquals(transactions[2].type, "deposit");
+  assertEquals(transactions[2].amount, 25.50);
+});
+
+Deno.test(function getTransactions_returns_withdrawal_transaction() {
+  const bankAccount = new BankAccount();
+  bankAccount.deposit(100);
+  bankAccount.withdraw(30);
+  const transactions = bankAccount.getTransactions();
+  assertEquals(transactions.length, 2);
+  assertEquals(transactions[0].type, "deposit");
+  assertEquals(transactions[0].amount, 100);
+  assertEquals(transactions[1].type, "withdrawal");
+  assertEquals(transactions[1].amount, 30);
+});
+
+Deno.test(function getTransactions_returns_mixed_transactions_in_order() {
+  const bankAccount = new BankAccount();
+  bankAccount.deposit(100);
+  bankAccount.deposit(50);
+  bankAccount.withdraw(30);
+  bankAccount.deposit(20);
+  bankAccount.withdraw(10);
+  const transactions = bankAccount.getTransactions();
+  assertEquals(transactions.length, 5);
+  assertEquals(transactions[0].type, "deposit");
+  assertEquals(transactions[0].amount, 100);
+  assertEquals(transactions[1].type, "deposit");
+  assertEquals(transactions[1].amount, 50);
+  assertEquals(transactions[2].type, "withdrawal");
+  assertEquals(transactions[2].amount, 30);
+  assertEquals(transactions[3].type, "deposit");
+  assertEquals(transactions[3].amount, 20);
+  assertEquals(transactions[4].type, "withdrawal");
+  assertEquals(transactions[4].amount, 10);
+});
+
+Deno.test(function transaction_includes_date() {
+  const bankAccount = new BankAccount();
+  const beforeDeposit = new Date();
+  bankAccount.deposit(100);
+  const afterDeposit = new Date();
+  const transactions = bankAccount.getTransactions();
+  assertEquals(transactions.length, 1);
+  assert(transactions[0].date instanceof Date);
+  assert(transactions[0].date >= beforeDeposit);
+  assert(transactions[0].date <= afterDeposit);
+});
+
+Deno.test(function transactions_are_in_chronological_order() {
+  const bankAccount = new BankAccount();
+  bankAccount.deposit(100);
+  bankAccount.withdraw(30);
+  bankAccount.deposit(50);
+  const transactions = bankAccount.getTransactions();
+  assertEquals(transactions.length, 3);
+  assert(transactions[0].date <= transactions[1].date);
+  assert(transactions[1].date <= transactions[2].date);
+});
+
+Deno.test(function transaction_records_balance_after_transaction() {
+  const bankAccount = new BankAccount();
+  bankAccount.deposit(100);
+  bankAccount.withdraw(30);
+  bankAccount.deposit(50);
+  const transactions = bankAccount.getTransactions();
+  assertEquals(transactions.length, 3);
+  assertEquals(transactions[0].balanceAfter, 100);
+  assertEquals(transactions[1].balanceAfter, 70);
+  assertEquals(transactions[2].balanceAfter, 120);
+});
+
